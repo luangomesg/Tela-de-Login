10c17c11047d137b35cafef97c556299f6477101
luanitachi <luanitachi@hotmail.com>
2023-01-19 16:30:43 -0300

    add funcionalidade no botão de registrar

---
+++ script.js
@@ -0,0 +1,21 @@
+let btnRegister = document.querySelector(".btn-register");
+let btnConfirm = document.querySelector(".btn-confirm");
+let login1 = document.querySelector(".container-login");
+let login2 = document.querySelector(".container-login2");
+
+btnRegister.addEventListener("click", seeRegister);
+btnConfirm.addEventListener("click", seeLogin);
+
+function seeRegister() {
+    login1.style.display = "none";
+    if(login1.style.display == "none") {
+        login2.style.display = "flex"
+    }
+}
+
+function seeLogin() {
+    if(login2.style.display == "flex") {
+        login2.style.display = "none";
+        login1.style.display = "flex";
+    }
+}
